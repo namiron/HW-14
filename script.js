f92050f656b30db2aad724a305f9fa35b3130454
namiron <102922048+namiron@users.noreply.github.com>
2023-08-08 19:17:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,623 @@
+
+
+// let dataList = [
+// 	{
+// 		id: 344277463,
+// 		old_price: 1395,
+// 		old_usd_price: '37.70',
+// 		price: 1099,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '29.70',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 363766641,
+// 		old_price: 0,
+// 		old_usd_price: '0.00',
+// 		price: 90,
+// 		min_month_price: 0,
+// 		sell_status: 'unavailable',
+// 		status: 'active',
+// 		usd_price: '2.43',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 339273715,
+// 		old_price: 38,
+// 		old_usd_price: '1.03',
+// 		price: 25,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '0.68',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 330746665,
+// 		old_price: 3087,
+// 		old_usd_price: '83.43',
+// 		price: 2548,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '68.86',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 344972806,
+// 		old_price: 699,
+// 		old_usd_price: '18.89',
+// 		price: 549,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '14.84',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 318302299,
+// 		old_price: 0,
+// 		old_usd_price: '0.00',
+// 		price: 8500,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '229.73',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 361430565,
+// 		old_price: 3500,
+// 		old_usd_price: '94.59',
+// 		price: 1999,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '54.03',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 14429030,
+// 		old_price: 3339,
+// 		old_usd_price: '90.24',
+// 		price: 2999,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '81.05',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 56340912,
+// 		old_price: 2094,
+// 		old_usd_price: '56.59',
+// 		price: 1776,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '48.00',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 315292225,
+// 		old_price: 1799,
+// 		old_usd_price: '48.62',
+// 		price: 1499,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '40.51',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 28437961,
+// 		old_price: 42999,
+// 		old_usd_price: '1162.14',
+// 		price: 33999,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '918.89',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 339896833,
+// 		old_price: 6399,
+// 		old_usd_price: '172.95',
+// 		price: 5899,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '159.43',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 364354149,
+// 		old_price: 1600,
+// 		old_usd_price: '43.24',
+// 		price: 1500,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '40.54',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 363184995,
+// 		old_price: 0,
+// 		old_usd_price: '0.00',
+// 		price: 4499,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '121.59',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 98077846,
+// 		old_price: 0,
+// 		old_usd_price: '0.00',
+// 		price: 3113,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '84.14',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 310694668,
+// 		old_price: 0,
+// 		old_usd_price: '0.00',
+// 		price: 3000,
+// 		min_month_price: 0,
+// 		sell_status: 'unavailable',
+// 		status: 'active',
+// 		usd_price: '81.08',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 362812029,
+// 		old_price: 0,
+// 		old_usd_price: '0.00',
+// 		price: 21700,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '586.49',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 361422708,
+// 		old_price: 4100,
+// 		old_usd_price: '110.81',
+// 		price: 2699,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '72.95',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 318302257,
+// 		old_price: 0,
+// 		old_usd_price: '0.00',
+// 		price: 8500,
+// 		min_month_price: 0,
+// 		sell_status: 'unavailable',
+// 		status: 'active',
+// 		usd_price: '229.73',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 310694498,
+// 		old_price: 0,
+// 		old_usd_price: '0.00',
+// 		price: 2963,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '80.08',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 363651273,
+// 		old_price: 5199,
+// 		old_usd_price: '140.51',
+// 		price: 4890,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '132.16',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 330747022,
+// 		old_price: 3087,
+// 		old_usd_price: '83.43',
+// 		price: 2606,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '70.43',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 362617635,
+// 		old_price: 4872,
+// 		old_usd_price: '131.68',
+// 		price: 2436,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '65.84',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// 	{
+// 		id: 363614439,
+// 		old_price: 5199,
+// 		old_usd_price: '140.51',
+// 		price: 4890,
+// 		min_month_price: 0,
+// 		sell_status: 'available',
+// 		status: 'active',
+// 		usd_price: '132.16',
+// 		pl_charge_pcs: 0,
+// 		pl_use_instant_bonus: false,
+// 		pl_premium_bonus_charge_pcs: 0,
+// 		rests: -1,
+// 		min_price: 0,
+// 		max_price: 0,
+// 		has_shops: false,
+// 		info: null,
+// 		show_in_site: null,
+// 	},
+// ]
+
+
+// // 1) Загальну вартість (нові ціни)
+// // 2)Знайти кількість товарів, у яких ціна зменшилась (price < old_price).
+// // 3) Товари, які доступні (sell_status:"available")
+// // 4) сформувати новий список об”єктів тільки доступних  для продажу товарів, які міститимуть тільки ідентифікатор товару (id), нову ціну (price), стару ціну (old_price), та ціну у доларах (usd_price)
+
+// //Загальну вартість (нові ціни)
+// let newPriceList = dataList.reduce((previousValue, price) => previousValue + price.price, 0)
+// console.log(newPriceList);
+// //2)Знайти кількість товарів, у яких ціна зменшилась (price < old_price).
+
+// let changePrice = dataList.reduce((previousValue, el) => el.price < el.old_price ? previousValue + 1 : previousValue, 0)
+// console.log(changePrice);
+
+// // 3) Товари, які доступні (sell_status:"available")
+
+// let statusObjectKay = dataList.reduce((previousValue, el) => el.sell_status === "available" ? previousValue + 1 : previousValue, 0)
+// console.log(statusObjectKay);
+
+
+// // 4) сформувати новий список об”єктів тільки доступних  для продажу товарів, які міститимуть тільки ідентифікатор товару (id), нову ціну (price), стару ціну (old_price), та ціну у доларах (usd_price)
+
+// /* let listSellingGoods = []
+// for (const dataUser of dataList) {
+// 	if (dataUser.sell_status === 'available') {
+// 		listSellingGoods.push({
+// 			id: dataUser.id,
+// 			price: dataUser.price,
+// 			old_price: dataUser.old_price,
+// 			usd_price: dataUser.usd_price,
+// 			sell_status: dataUser.sell_status,
+// 		})
+// 	}
+
+
+// }
+// console.log(listSellingGoods); */
+
+
+// //================================================================================
+// // 4) сформувати новий список об”єктів тільки доступних  для продажу товарів, які міститимуть тільки ідентифікатор товару (id), нову ціну (price), стару ціну (old_price), та ціну у доларах (usd_price)
+
+// let listSellingGoods = []
+
+// const goodsData = ['id:dataList.id', 'price:dataList.price', 'old_price:dataList.price', 'usd_price:dataList.price'];
+
+// for (const dataClient of dataList) {
+// 	for (const key in dataClient) {
+// 		if (goodsData.includes(key)) {
+// 			dataClient[key] = goodsData[key];
+// 		}
+// 	}
+// 	if (dataClient.sell_status === 'available') {
+// 		listSellingGoods.push(dataClient)
+// 	}
+
+// }
+// console.log(listSellingGoods);
+
+// /* let listSellingGoods = []
+// const goodsData = ["id", "price", "old_price", "usd_price"]
+// for (const dataClient of dataList) {
+// 	if (dataClient.sell_status === "available") {
+// 		let currentProd = {}
+// 		for (const key in dataClient) {
+// 			if (goodsData.includes(key)) {
+// 				currentProd[key] = dataClient[key]
+// 			}
+// 		}
+// 		listSellingGoods.push(currentProd)
+// 	}
+// } */
+// //==============================================================================
+
+// // Задача 1. Описати масив об’єктів – сайтів розроблених компанією з такими властивостями
+// // ----- Властивості ------
+// // назва компанії на час розробки (назву періодично змінюють)
+// // власник компанії
+// // споснсори (масив спонсорів)
+// //       * прізвище спонсора
+// //       * ім’я  спонсора
+// //       * сума вкладень спонсора
+// // рік випуску
+// // вартість сайту
+// // Знайти:
+// // 1) загальну вартість усіх сайтів
+// // 2) кількість сайтів, що було зроблено між 2000 та 2009 рр.
+// // 3) кількість сайтів, де сума спонсорських вкладень була більшою за 100000
+// // 4) створити загальний список усіх спонсорів (поки можуть повторюватись, просто зібрати усі у масив)
+// // 5) знайти рік, коли прибуток був найбільшим
+// // 6) упорядкувати список за спаданням прибутку
+
+
+// // let nameFirstSite = prompt("введіть назву  першрго сайту");
+// // let nameSecondSite = prompt("введіть назву другого сайту");
+// // let nameThirdSite = prompt("введіть назву третого сайту");
+// // let nameFourthSite = prompt("введіть назву четвертого сайту");
+
+// // let sitesLatter = [
+// // 	{
+// // 		title: nameFirstSite,
+// // 		sponsor: {
+// // 			name: 'Anastasia',
+// // 			investmentAmountYear: 100,
+// // 		},
+// // 		yearIssue: 2000,
+// // 		siteCost: 400,
+// // 	},
+// // 	{
+// // 		title: nameSecondSite,
+// // 		sponsor: {
+// // 			name: 'Alex',
+// // 			investmentAmountYear: 100001,
+// // 		},
+// // 		yearIssue: 2005,
+// // 		siteCost: 2000,
+// // 	},
+// // 	{
+// // 		title: nameThirdSite,
+// // 		sponsor: {
+// // 			name: 'Sofia',
+// // 			investmentAmountYear: 900,
+// // 		},
+// // 		yearIssue: 2007,
+// // 		siteCost: 1800,
+// // 	},
+// // 	{
+// // 		title: nameFourthSite,
+// // 		sponsor: {
+// // 			name: 'Alexander',
+// // 			investmentAmountYear: 600,
+// // 		},
+// // 		yearIssue: 2009,
+// // 		siteCost: 1000,
+// // 	},
+// // ]
+// // // 1) загальну вартість усіх сайтів
+// // let generalPriceSum = sitesLatter.reduce((previousValue, price) => previousValue + price.siteCost, 0);
+// // console.log(generalPriceSum);
+// // // 2) кількість сайтів, що було зроблено між 2000 та 2009 рр.
+// // let filterSiteSum = sitesLatter.reduce((previousValue, years) => years.yearIssue > 2000 && years.yearIssue < 2009 ? previousValue + 1 : previousValue, 0)
+// // console.log(filterSiteSum);
+// // // 3) кількість сайтів, де сума спонсорських вкладень була більшою за 100000
+// // let LargeAmountInvestment = sitesLatter.reduce((previousValue, invest) => invest.sponsor.investmentAmountYear > 100000 ? previousValue + 1 : previousValue, 0)
+// // console.log(LargeAmountInvestment);
+
+// // // 4) створити загальний список усіх спонсорів (поки можуть повторюватись, просто зібрати усі у масив)
+// // let sponsors = []
+
+// // for (const item of sitesLatter) {
+// // 	sponsors.push(item.sponsor);
+
+// // }
+// // console.log(sponsors);
+
+
+
+let randomResults = (max, min) => min + Math.floor(Math.random() * max);
+
+
+
+console.log(randomResults(1000000, 1)); 
